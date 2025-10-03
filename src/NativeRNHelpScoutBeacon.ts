62b184b000cd90ed1c5cc1044a06bcdbe7c1f11c
Justin Stanley <justin.stanley@gmail.com>
2025-10-03 12:52:30 -0500

    fix invalid codegen while processing podfile

--- src/NativeRNHelpScoutBeacon.ts
+++ src/NativeRNHelpScoutBeacon.ts
@@ -1,28 +1,27 @@
-import { TurboModule, TurboModuleRegistry } from 'react-native';
+import { TurboModule, TurboModuleRegistry } from 'react-native'
 
 export interface Identity {
-  email?: string;
-  name?: string;
-  [key: string]: string | undefined;
+	email?: string
+	name?: string
 }
 
 export interface Spec extends TurboModule {
-  readonly init: (beaconId: string) => void;
-  readonly open: (signature?: string) => void;
-  readonly identify: (identity: Identity) => void;
-  readonly logout: () => void;
-  readonly navigate: (route: string) => void;
-  readonly search: (query: string) => void;
-  readonly openArticle: (articleId: string) => void;
-  readonly contactForm: () => void;
-  readonly previousMessages: () => void;
-  readonly dismiss: () => Promise<void>;
-  readonly prefillForm: (subject: string, content: string) => void;
-  readonly clearFormPrefill: () => void;
-  
-  // Event emitter support
-  readonly addListener: (eventName: string) => void;
-  readonly removeListeners: (count: number) => void;
+	readonly init: (beaconId: string) => void
+	readonly open: (signature?: string) => void
+	readonly identify: (identity: Identity) => void
+	readonly logout: () => void
+	readonly navigate: (route: string) => void
+	readonly search: (query: string) => void
+	readonly openArticle: (articleId: string) => void
+	readonly contactForm: () => void
+	readonly previousMessages: () => void
+	readonly dismiss: () => Promise<void>
+	readonly prefillForm: (subject: string, content: string) => void
+	readonly clearFormPrefill: () => void
+
+	// Event emitter support
+	readonly addListener: (eventName: string) => void
+	readonly removeListeners: (count: number) => void
 }
 
-export default TurboModuleRegistry.get<Spec>('RNHelpScoutBeacon') as Spec | null;
+export default TurboModuleRegistry.get<Spec>('RNHelpScoutBeacon') as Spec | null
